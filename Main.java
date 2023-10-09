c72bf1cf29eeddda2573bcf41d01876790c0eb0b
Gonzalo <gonzarf21@gmail.com>
2023-10-09 10:13:15 +0200

    Main prints farawell

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 public static void Main(String[] args){
 	String salutation = "Hola mundo";
 	System.outprintln(salutation);
+        String farawell = "Adios con el corazon";
+	System.outprintln(farawell);
 }
