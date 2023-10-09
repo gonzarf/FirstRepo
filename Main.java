9e5105c6d8a4f2e05be2cd58372417144bbe31b1
Gonzalo <gonzarf21@gmail.com>
2023-10-09 10:26:10 +0200

    Main now has an addition

--- Main.java
+++ Main.java
@@ -3,4 +3,12 @@ public static void Main(String[] args){
 	System.outprintln(salutation);
         String farawell = "Adios con el corazon";
 	System.outprintln(farawell);
+	
+	int num1 = 4;
+	int num2 = 5;
+	System.outprintln(addNumber(num1,num2));
+
+}
+public static int addNumbers(int a, int b){
+	return(a+b);
 }
